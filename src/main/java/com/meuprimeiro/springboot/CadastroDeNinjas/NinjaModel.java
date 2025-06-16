e34d01b8750c07c066f5b21f790695237498e196
Cleison Alves <142464083+TchecoAlves@users.noreply.github.com>
2025-06-16 20:43:15 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

---
+++ src/main/java/com/meuprimeiro/springboot/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,51 @@
+package com.meuprimeiro.springboot.CadastroDeNinjas;
+
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+// Anotação @Entity indica que essa classe é uma entidade JPA do BD
+@Entity
+@Table(name = "tb_cadastro") // Anotação @Table define o nome da tabela no banco de dados
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
